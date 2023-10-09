c7d6530856a438f7d71b65869e7e2201765599de
BEECHARLA VAMSHIDHAR REDDY <87815097+B-VAMSHIDHARREDDY@users.noreply.github.com>
2023-10-09 14:55:29 +0530

    Update urls.py

--- shelf_api/urls.py
+++ shelf_api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ShelfIdentificationView
 
 urlpatterns = [
-    path('', ShelfIdentificationView.as_view(), name='identify-shapes'),
+    path('shelf-identification/', ShelfIdentificationView.as_view(), name='identify-shapes'),
 ]
